7a53e3a8eb01a99cb41146cc82c442827e490398
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2017-09-12 18:35:41 +0200

    es6 yo

--- iconCreator.js
+++ iconCreator.js
@@ -1,180 +1,163 @@
-var async = require('async'),
-    request = require('request'),
-    fs = require('fs'),
-    imagemagick = require('imagemagick'),
-    gm = require('gm').subClass({ imageMagick: true }),
-    exec = require('child_process').exec;
+const async = require('async')
+const request = require('request')
+const fs = require('fs')
+const imagemagick = require('imagemagick')
+const gm = require('gm').subClass({ imageMagick: true })
+const exec = require('child_process').exec
 
-var iconCreator = function(name){
-    this.name = name.split(" ").join("");
-    this.path = "./icons/";
-    this.filePath = null;
-    this.filePath_bmp = null;
-    this.filePath_svg = null;
-    this.url = null;
-    this.icon = null;
-    this.operationQueue = async.queue(function (task, callback) {
-        console.log('item pushed');
-        task(callback);
-    }, 1);
-    return this;
-};
+const iconCreator = (name) => {
+  this.name = name.split(" ").join("")
+  this.path = "./icons/"
+  this.filePath = null
+  this.filePath_bmp = null
+  this.filePath_svg = null
+  this.url = null
+  this.icon = null
+  this.operationQueue = async.queue((task, callback) => {
+    console.log('item pushed')
+    task(callback)
+  }, 1)
+  return this
+}
 
-iconCreator.prototype.download = function(url){
-    console.log("iconCreator.prototype.download call");
-    var self = this;
-    self.url = url;
+iconCreator.prototype.download = (url) => {
+  console.log("iconCreator.prototype.download call")
+  const self = this
+  self.url = url
 
-    self.operationQueue.push(function(queueCallback){
-        self.icon = gm(request(self.url)).stream(function (err, stdout, stderr) {
-            self.filePath = self.path + self.name + ".png";
-            var writeStream = fs.createWriteStream(self.filePath);
-            stdout.pipe(writeStream);
+  self.operationQueue.push(queueCallback => {
+    self.icon = gm(request(self.url)).stream((err, stdout, stderr) => {
+      self.filePath = self.path + self.name + ".png"
+      var writeStream = fs.createWriteStream(self.filePath)
+      stdout.pipe(writeStream)
 
-            writeStream.on('error', function(e){
-                console.log(e);
-                queueCallback();
-            });
+      writeStream.on('error', err => {
+        console.log(err)
+        queueCallback()
+      })
 
-            writeStream.on('finish', function(){
-                console.log("download finish")
-                queueCallback();
-            });
-            stdout.on('error', function(err){
-                console.log('err: ' + err);
-                queueCallback();
-            });
-        });
-    });
-    return self;
-};
+      writeStream.on('finish', () => {
+        console.log("download finish")
+        queueCallback()
+      })
+      stdout.on('error', err => {
+        console.log('err: ' + err)
+        queueCallback()
+      })
+    })
+  })
+  return self
+}
 
-iconCreator.prototype.contrast = function(amount){
-    console.log("iconCreator.prototype.contrast call");
-    var self = this;
-    self.operationQueue.push(function(queueCallback){
-        amount = amount ? amount : 10;
-        var cute = "convert " + self.filePath + " -fuzz " + amount + "% -fill black +opaque black " + self.filePath;
-        exec(cute, function (error, stdout, stderr) {
-            if (error || stderr){
-                console.log(error || stderr);
-            }
-            queueCallback();
-        });
-    });
-    return self;
-};
+iconCreator.prototype.contrast = amount => {
+  console.log("iconCreator.prototype.contrast call")
+  const self = this
+  self.operationQueue.push(queueCallback => {
+    amount = amount ? amount : 10
+    const cute = `convert ${self.filePath} -fuzz ${amount} % -fill black +opaque black ${self.filePath}`
+    exec(cute, function (error, stdout, stderr) {
+      if (error || stderr) {
+        console.log(error || stderr)
+      }
+      queueCallback()
+    })
+  })
+  return self
+}
 
-iconCreator.prototype.monochrome = function(){
-    var self = this;
-    self.operationQueue.push(function(queueCallback){
-        var cute = "convert " + self.filePath + " -fill black +opaque black " + self.filePath;
-        exec(cute, function (error, stdout, stderr) {
-            if (error || stderr){
-                console.log(error || stderr);
-            }
-            queueCallback();
-        });
-    });
-    return self;
-};
+iconCreator.prototype.monochrome = () => {
+  const self = this
+  self.operationQueue.push(queueCallback => {
+    const cute = `convert ${self.filePath} -fill black +opaque black ${self.filePath}`
+    exec(cute, function (error, stdout, stderr) {
+      if (error || stderr) {
+        console.log(error || stderr)
+      }
+      queueCallback()
+    })
+  })
+  return self
+}
 
-iconCreator.prototype.checkColor = function(x, y){
-    var self = this;
-    self.operationQueue.push(function(queueCallback){
-        x = x ? x : 1;
-        y = y ? y : 1;
-        var cute = "convert " + self.filePath + " -scale " + x + "x" + y + "\! -format \'%[pixel:u]\' info:-";
-        exec(cute, function (error, stdout, stderr) {
-            if (error || stderr){
-                console.log(error || stderr);
-            }
-            if (stdout === 'gray(0)' || stdout === 'srgb(0,0,0)'){
-                self.abort = true;
-            }
-            queueCallback();
-        });
-    });
-    return self;
-};
+iconCreator.prototype.checkColor = (x, y) => {
+  const self = this
+  self.operationQueue.push(queueCallback => {
+    x = x ? x : 1
+    y = y ? y : 1
+    const cute = `convert ${self.filePath} -scale ${x}x ${y}y \! -format '%[pixel:u]' info:-`
+    exec(cute, function (error, stdout, stderr) {
+      if (error || stderr) {
+        console.log(error || stderr)
+      }
+      if (stdout === 'gray(0)' || stdout === 'srgb(0,0,0)') {
+        self.abort = true
+      }
+      queueCallback()
+    })
+  })
+  return self
+}
 
-iconCreator.prototype.resizeAndCenter = function(size){
-    var self = this;
-    self.operationQueue.push(function(queueCallback){
-        size = size ? size : "220x220";
-        var cute = "convert -define png:size=" + size + " " + self.filePath + " -thumbnail \"200x200\" -background transparent -gravity center -extent 220x220 " + self.filePath;
-        exec(cute, function (error, stdout, stderr) {
-            if (error || stderr){
-                console.log(error || stderr);
-            }
-            queueCallback();
-        });
-    });
-    return self;
-};
+iconCreator.prototype.resizeAndCenter = size => {
+  const self = this
+  self.operationQueue.push(queueCallback => {
+    size = size ? size : "220x220"
+    const cute = `convert -define png:size=${size} ${self.filePath} -thumbnail \"200x200\" -background transparent -gravity center -extent 220x220 ${self.filePath}`
+    exec(cute, (error, stdout, stderr) => {
+      if (error || stderr) {
+        console.log(error || stderr)
+      }
+      queueCallback()
+    })
+  })
+  return self
+}
 
-iconCreator.prototype.toBmp = function(){
-    var self = this;
-    self.operationQueue.push(function(queueCallback){
-        self.filePath_bmp = self.filePath.replace(".png", ".bmp");
-        var cute = "convert " + self.filePath +  " -background \"#FFFFFF\" -flatten " + self.filePath_bmp;
-        exec(cute, function (error, stdout, stderr) {
-            if (error || stderr){
-                console.log(error || stderr);
-            }
-            queueCallback();
-        });
-    });
-    return self;
-};
+iconCreator.prototype.toBmp = () => {
+  const self = this
+  self.operationQueue.push(queueCallback => {
+    self.filePath_bmp = self.filePath.replace(".png", ".bmp")
+    const cute = `convert ${self.filePath} -background \"#FFFFFF\" -flatten ${self.filePath_bmp}`
+    exec(cute, (error, stdout, stderr) => {
+      if (error || stderr) {
+        console.log(error || stderr)
+      }
+      queueCallback()
+    })
+  })
+  return self
+}
 
-iconCreator.prototype.toSvg = function(){
-    var self = this;
-    self.operationQueue.push(function(queueCallback){
-        self.filePath_svg = self.filePath_bmp.replace(".bmp", ".svg");
-        var cute = "potrace -s -o " + self.filePath_svg + ' ' + self.filePath_bmp;
-        exec(cute, function (error, stdout, stderr) {
-            if (error || stderr){
-                console.log(error || stderr);
-            }
-            queueCallback();
-        });
-    });
-    return self;
-};
+iconCreator.prototype.toSvg = () => {
+  const self = this
+  self.operationQueue.push(queueCallback => {
+    self.filePath_svg = self.filePath_bmp.replace(".bmp", ".svg")
+    const cute = `potrace -s -o ${self.filePath_svg} ${self.filePath_bmp}`
+    exec(cute, (error, stdout, stderr) => {
+      if (error || stderr) {
+        console.log(error || stderr)
+      }
+      queueCallback()
+    })
+  })
+  return self
+}
 
-iconCreator.prototype.pixelate = function(filter, scale, threshold){
-    var self = this;
-    self.operationQueue.push(function(queueCallback){
-        filter = filter ? filter : 1;
-        scale = scale ? scale : 2;
-        threshold = threshold ? threshold : 0.5;
-        var cute = "mkbitmap " + self.filePath_bmp + " -f " + filter + " -s " + scale + " -t " + threshold + " -o " + self.filePath_bmp;
-        exec(cute, function (error, stdout, stderr) {
-            if (error || stderr){
-                console.log(error || stderr);
-            }
-            queueCallback();
-        });
-    });
-    return self;
-};
+iconCreator.prototype.pixelate = (filter, scale, threshold) => {
+  const self = this
+  self.operationQueue.push(queueCallback => {
+    filter = filter ? filter : 1
+    scale = scale ? scale : 2
+    threshold = threshold ? threshold : 0.5
+    const cute = `mkbitmap ${self.filePath_bmp} -f ${filter} -s ${scale} -t ${threshold} -o ${self.filePath_bmp}`
+    exec(cute, (error, stdout, stderr) => {
+      if (error || stderr) {
+        console.log(error || stderr)
+      }
+      queueCallback()
+    })
+  })
+  return self
+}
 
-// iconCreator.prototype.getPixelMatrix = function(width){
-//     // we always assume we're dealing with a square image.
-//     var self = this;
-//     self.pixelMatrix = [
-//         [],
-//         []
-//     ];
-//
-//     for (var i = 0; i < width; i++) {
-//         array[0].push(i);
-//         array[1].push(i);
-//     };
-//
-//
-// };
-
-
-module.exports = iconCreator;
+module.exports = iconCreator
